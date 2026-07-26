459ba8aacc8ddd0627775f426473cb6ecf328c6a
Rasik2504 <rasikrahman2504@gmail.com>
2026-07-26 17:04:55 +0530

    Program 1 uploaded

---
+++ basic/program1.py
@@ -0,0 +1,8 @@
+name=input("Enter The Student name:")
+mark1=int(input("Enter The mark1:"))
+mark2=int(input("Enter The mark2:"))
+mark3=int(input("Enter The mark3:"))
+total=mark1+mark2+mark3
+print("The total mark is:", total)
+average=total/3
+print("The average mark is:", average)
