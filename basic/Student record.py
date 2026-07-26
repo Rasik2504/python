6dcbe0d5047ae8bb61fc740f9c32677dbb139a64
Anishagit1806 <anishaaids18@gmail.com>
2026-07-26 19:35:58 +0530

    Program 1 updated

--- basic/Student record.py
+++ basic/Student record.py
@@ -2,7 +2,11 @@ name=input("Enter The Student name:")
 mark1=int(input("Enter The mark1:"))
 mark2=int(input("Enter The mark2:"))
 mark3=int(input("Enter The mark3:"))
-total=mark1+mark2+mark3
+mark4=int(input("Enter The mark4:"))
+mark5=int(input("Enter The mark5:"))
+total=mark1+mark2+mark3+mark4+mark5
 print("The total mark is:", total)
-average=total/3
-print("The average mark is:", average)
+average=total/5
+print("The average mark is:", round(average,2))
+percentage=(total/500)*100
+print("The percentage is:", percentage)
