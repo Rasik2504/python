6f5977789bc98e2343060da7242ecb961800e706
Anishagit1806 <anishaaids18@gmail.com>
2026-07-29 12:07:23 +0530

    Program uploaded

---
+++ pattern/Diamond.py
@@ -0,0 +1,13 @@
+n=int(input("Enter a number:"))
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for j in range(2*i):
+        print("*",end="")
+    print("*")
+for i in range(n):
+    for j in range(i):
+        print(" ",end="")
+    for j in range(2*(n-i)-1):
+        print("*",end="")
+    print()
