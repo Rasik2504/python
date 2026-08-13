7e2c14d6017f3815b826891f6d616a003d39b877
Rasik2504 <rasikrahman2504@gmail.com>
2026-08-13 19:37:09 +0530

    Program Added

---
+++ oops_inheritance/multiple_inheritance.py
@@ -0,0 +1,30 @@
+# PRACTICE — MULTIPLE INHERITANCE
+#
+# 1. Create class A.
+#
+#    Add:
+#    show()
+#    Print "Class A"
+#
+# 2. Create class B.
+#
+#    Add:
+#    show()
+#    Print "Class B"
+#
+# 3. Create class C(A, B).
+#
+# 4. Create object:
+#    c1 = C()
+#
+# 5. Call:
+#    c1.show()
+#
+# 6. Print:
+#    C.mro()
+#
+# 7. Identify which show() Python executes first.
+#
+# 8. Change C(A, B) to C(B, A).
+#
+# 9. Run again and observe the difference.
---
+++ oops_inheritance/single+multilevel.py
@@ -0,0 +1,24 @@
+# PRACTICE
+#
+# Create a class Vehicle.
+# Add method start().
+#
+# Create class Car(Vehicle).
+# Add method drive().
+#
+# Create class ElectricCar(Car).
+# Add method charge().
+#
+# Create an ElectricCar object.
+#
+# Call:
+# start()
+# drive()
+# charge()
+#
+# Identify:
+# Vehicle = ?
+# Car = ?
+# ElectricCar = ?
+#
+# What type of inheritance is this?
